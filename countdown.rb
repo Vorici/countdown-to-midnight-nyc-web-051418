58fc528a8063d3ce7d25694fa0ede18de7317658
Ledio Bode <l.bode515@gmail.com>
2018-05-11 04:04:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,18 @@
 
 
 def countdown(integer)
-while integer > 0
+ while integer > 0
   puts "#{integer} SECOND(S)!"
-  integer -= 1
-end
+   integer -= 1
+ end
  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(count)
- while count > 0
-      count-=1
-      sleep(1)
+
+def countdown_with_sleep(integer)
+ while integer > 0
+     integer-=1
+      sleep(5)
     end
       puts "0 left. HAPPY NEW YEAR!"
   end
